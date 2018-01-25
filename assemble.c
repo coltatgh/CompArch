f7283ebbc162e02e36ac96a2d5a2d3ef59f1917b
karlsolomon <ksolomon@utexas.edu>
2018-01-24 20:51:59 -0600

    working generic logic, need to add each command to switch statement now

--- assemble.c
+++ assemble.c
@@ -1,8 +1,18 @@
+/*
+1st pass converts everything into uppercase? 
+*/
+
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <malloc.h>
 
+#define ASCIINUM '0'
+#define ASCIIUPPER ('A' - 10)
+#define ASCIILOWER ('a' - 10)
+#define SIGNED  1
+#define UNSIGNED 0
 
 //begin Source 1 = https://stackoverflow.com/questions/9907160/how-to-convert-enum-names-to-string-in-c
 #define FOREACH_OPCODE(OPCODE) \
@@ -34,8 +44,21 @@
         OPCODE(TRAP)   \
         OPCODE(XOR)   \
 
+#define FOREACH_PSEUDOOP(PSEUDOOP) \
+        PSEUDOOP(ORIG)   \
+        PSEUDOOP(FILL)  \
+        PSEUDOOP(END)   \
+
 #define GENERATE_ENUM(ENUM) ENUM,
 #define GENERATE_STRING(STRING) #STRING,
+
+enum PSEUDOOP {
+    FOREACH_PSEUDOOP(GENERATE_ENUM)
+};
+
+static const char *pseudoOps[] = {
+    FOREACH_PSEUDOOP(GENERATE_STRING)
+};
 
 enum OPCODE {
     FOREACH_OPCODE(GENERATE_ENUM)
@@ -44,22 +67,58 @@ enum OPCODE {
 static const char *opcodes[] = {
     FOREACH_OPCODE(GENERATE_STRING)
 };
+
+static const uint8_t opcodeMap[] = {
+    1,    //OPCODE(ADD)   
+    5,    //OPCODE(AND)  
+    0,    //OPCODE(BR)  
+    0,    //OPCODE(BRN)   
+    0,    //OPCODE(BRZ)   
+    0,    //OPCODE(BRP)   
+    0,    //OPCODE(BRNZ)   
+    0,    //OPCODE(BRNP)   
+    0,    //OPCODE(BRZP)   
+    0,    //OPCODE(BRNZP)   
+    0,    //OPCODE(HALT)   
+    12,    //OPCODE(JMP)   
+    4,    //OPCODE(JSR)   
+    4,    //OPCODE(JSRR)   
+    2,    //OPCODE(LDB)   
+    4,    //OPCODE(LDW)   
+    14,    //OPCODE(LEA)   
+    0,    //OPCODE(NOP)   
+    9,    //OPCODE(NOT)   
+    12,    //OPCODE(RET)   
+    13,    //OPCODE(RSHFL)   
+    13,    //OPCODE(RSHFA)   
+    8,    //OPCODE(RTI)   
+    3,    //OPCODE(STB)   
+    7,    //OPCODE(STW)   
+    15,    //OPCODE(TRAP)   
+    9    //OPCODE(XOR)   
+};
+
 //end SOURCE1
 
 //GLOBAL VARIABLES
-FILE *fileIn;
-FILE *fileOut;
-char errorMessage[100];
+FILE *fileIn = NULL;
+FILE *fileOut = NULL;
+char errorMessage[120];
+uint32_t lineNumber = 1;
+uint16_t result = 0;
 
 //FUNCTION PROTOTYPES
 bool isLabelValid(char* label);
-int getOpcode(char* opcode);
-
-
-
-
+enum OPCODE getOpcode(char* opcode);
+int getPseudoOp(char* pseudoOp);
+void output(uint16_t output);
+int hexToInt(char* hex);
+int decToInt(char* dec);
+uint16_t opcodeToASM(enum OPCODE opcode);
+uint16_t RegisterToInt(char* reg);
 
 void error(int32_t errorCode, char* extraMessage) {
+  printf("\nLINE #%d\t", lineNumber);
   switch(errorCode) {
     case 0:
     case 1:
@@ -78,15 +137,19 @@ void error(int32_t errorCode, char* extraMessage) {
       break;
   }
   if(extraMessage) {
-    printf(extraMessage);
+    printf("%s",extraMessage);
   }
   printf("\n");
-  //exit(errorCode);
+  // if(fileOut != NULL) {
+  //   fclose(fileOut);
+  // }
+  // fileOut = fopen("out.obj", "w"); //TODO: change to non-magic string
+  // fputs("", fileOut); //clear contents of the output file if there is an error
+  // fclose(fileOut);
+  // exit(errorCode);
 }
 
-
-
-void main(void) {
+void main_testLabel(void) {
 	char* label1 = "AND";
 	char* label2 = "$AND1";
 	char* label3 = "AND1";
@@ -103,6 +166,121 @@ void main_testGetOpcode(void) {
 	printf("%d %d %d %d", getOpcode(opcode1), getOpcode(opcode2), getOpcode(opcode3), getOpcode(opcode4));
 }
 
+void main_TestParse1(void) {
+  char* pOp1 = ".ORIG";
+  char* pOp2 = ".FILL";
+  char* pOp3 = ".END";
+  char* pOp4 = "HALT";
+  printf("PSEUDO\n%d %d %d %d", getPseudoOp(pOp1), getPseudoOp(pOp2), getPseudoOp(pOp3), getPseudoOp(pOp4));
+
+  printf("HEX\n%d %d %d %d", hexToInt("x-1000"), hexToInt("a8909"), hexToInt("xBEEF"), hexToInt("xBEEFF"));
+
+  printf("DEC\n%d %d %d %d", decToInt("#-1000"), decToInt("x8909"), decToInt("#1000"), decToInt("#65000"));
+}
+
+
+
+/*
+By the time this function is called the user is certain that the literal token is a numeric literal.
+The caller should know how many bits this number can occupy and whether or not that number is signed or unsigned
+*/
+int toLiteral(char* literal, uint8_t maxBits, bool isSigned) {
+  int num = 0;
+  int i;
+  int max = 1 << maxBits;
+  bool throwError = false;
+  if(literal[0] == 'x') {
+    num = hexToInt(literal+1); //skip the x
+  } else if (literal[0] == '#') {
+    num = decToInt(literal+1);
+  } else {
+    sprintf(errorMessage, "%s literal is not formatted correctly", literal);
+    throwError = true;
+  }
+  // check if within bounds
+  if(isSigned) {
+    if(num >= max) {
+      sprintf(errorMessage, "%s literal is too large. Max = %d", literal, max-1);
+      throwError = true;
+    }
+  } else {
+    if(num < 0) {
+      if(num*-2 > max) {
+        sprintf(errorMessage, "%s literal is too low. Min = %d. ", literal, (max>>1)*-1);
+        throwError = true;
+      }
+    } else {
+      if(2*num >= max) {
+        sprintf(errorMessage, "%s literal is too high. Max = %d", literal, (max>>1)-1);
+        throwError = true;
+      }
+    }
+  }
+  if(throwError) {
+    error(3, errorMessage);
+  }
+}
+
+/*
+call to distinguish between literal and register for Opcodes with options for either
+*/
+bool isLiteral(char* literal) {
+  return (literal[0] == 'x' || literal[0] == '#');
+}
+
+void main(int32_t argc, char* argv[]) {
+  int i;
+  if(argc == 3) {
+    fileIn = fopen(argv[1], "r");
+    fileOut = fopen(argv[2], "w");
+  } else {
+    error(4, "");
+    printf("expected ./assemble <input.asm> <output.obj>\n");
+  }
+
+  ssize_t line_size = 0;
+  ssize_t line_buf_size = 0;
+  char *token = malloc(10);
+  char *string;
+  char *delimiters = ", \t\n";
+  char *sr1 = malloc(10);
+  char *sr2 = malloc(10);
+  char *dr = malloc(10);
+
+  line_size = getline(&string, &line_buf_size, fileIn);
+  string[strlen(string) - 1] = '\0'; // getline adds a newline character after the string. remove that.
+  while(line_size != -1) {
+    result = 0;
+    token = strtok(string, delimiters);
+    enum OPCODE opcode = getOpcode(token);
+    result = opcodeToASM(opcode);
+    switch(opcode) {
+      case ADD:
+        dr = strtok(NULL, delimiters);
+        result += (RegisterToInt(dr) << 9);
+        sr1 = strtok(NULL, delimiters);
+        result += (RegisterToInt(sr1) << 6);
+        sr2 = strtok(NULL, delimiters);
+
+        printf("%s %s %s\n", dr, sr1, sr2);
+        if(isLiteral(sr2)) {
+          result += toLiteral(sr2, 5, SIGNED);
+          result += 1<<5;
+        } else {
+          result += RegisterToInt(sr2);
+        }
+        break;        
+      default:
+        sprintf(errorMessage, "1st token = %s", token);
+        break;
+    }
+    output(result);
+    line_size = getline(&string, &line_buf_size, fileIn);
+    lineNumber++;
+  }
+  free(string);
+  fclose(fileOut);
+}
 
 void main_testOutput(void) {
 	output(3000);
@@ -111,19 +289,40 @@ void main_testOutput(void) {
 	output(0);
 }
 
-// void main_testIO(int32_t argc, char* argv[]) {
-// 	int i;
-//   if(argc == 3) {
-//     fileIn = fopen(argv[1], "r");
-//     fileOut = fopen(argv[2], "w");
-//   } else {
-//     error(4, "")
-//     printf("expected ./assemble <input.asm> <output.obj>\n");
-//   }
-//   main_testOutput();
-//   fclose(fileIn);
-//   fclose(fileOut);
-// }
+void main_testIO(int32_t argc, char* argv[]) {
+  int i;
+  if(argc == 3) {
+    fileIn = fopen(argv[1], "r");
+    fileOut = fopen(argv[2], "w");
+  } else {
+    error(4, "");
+    printf("expected ./assemble <input.asm> <output.obj>\n");
+  }
+ fclose(fileIn);
+ fclose(fileOut);
+}
+
+/*
+returns the enum for the pseudoOp if it is one, else -1
+*/
+int getPseudoOp(char* pseudoOp) {
+  int code = -1;
+  int i = 0;
+  if(pseudoOp[0] != '.') {
+    sprintf(errorMessage, "%s is not a valid pseudoOp", pseudoOp);
+    error(2, errorMessage);
+  } else {
+    pseudoOp+=1; // skip the "."
+    while(pseudoOps[i] != NULL) {
+      if(strcmp(pseudoOps[i], pseudoOp) == 0) {
+        code = i;
+        break;
+      } 
+      i++;
+    }
+  }
+  return code;
+}
 
 /*
 Checks what is expected to be a label. This is used in the first pass, before storing a label in the label map.
@@ -163,17 +362,35 @@ bool isLabelValid(char* label) {
 Looks up what is expected to be an opcode in the opcode library.
 If it matches a known opcode it will return that opcode's enum value, else -1
 */
-int getOpcode(char* opcode) {
-  int code = -1;
-  int i = 0;
+enum OPCODE getOpcode(char* opcode) {
+  int8_t code = -1;
+  int8_t i = 0;
   while(opcodes[i] != NULL) {
-	if(strcmp(opcodes[i], opcode) == 0) {
-	  code = i;
-	  break;
-	} 
-	i++;
+  	if(strcmp(opcodes[i], opcode) == 0) {
+  	  code = i;
+  	  break;
+  	} 
+  	i++;
   }
   return code;
+}
+
+uint16_t RegisterToInt(char* reg)  {
+  int number = decToInt(reg+1); // skip the R
+  if(number > 7 || number < 0 || (reg[0] != 'r' && reg[0] != 'R')) {
+    sprintf(errorMessage, "%s is not a valid register, should be R0-R7", reg);
+    error(4, errorMessage);
+  }
+  return (uint16_t) number;
+}
+
+/*
+convert a known opcode into the assembly offset number that it should be encoded into
+*/
+uint16_t opcodeToASM(enum OPCODE opcode) {
+  uint16_t assembly = (uint16_t) opcodeMap[opcode];
+  assembly = assembly << 12;
+  return assembly;
 }
 
 /*
@@ -182,13 +399,82 @@ Formats output. Expects output to be a uint16_t. Will convert that number to a s
 2. prints every entry as a 4 digit hex in all caps
 */
 void output(uint16_t output) {
-	char* string [8];
-	sprintf(string, "0x%04X\n", output);
+	char string [8];
+  sprintf(string, "0x%04X\n", output);
 	fputs(string, fileOut);
 }
 
+/*
+case insensitive convert to int
+start with "-####" and convert to int
+no bounds checking yet
+*/
+int hexToInt(char* hex) {
+  int i;
+  int val = 0;
+  int end = 0;
+  int scalar = 1;
+  int constant;
+  int length = strlen(hex);
+  bool negative = (hex[0] == '-');
+  if(negative) {
+    end = 1;
+  }
 
+  sprintf(errorMessage, "%s is a bad hex literal. Needs form of \"x-####\" - optional",hex);
+  for(i = strlen(hex)-1; i >= end; i--) {  // end skips the 'x' (and if negative '-') character(s)
+    if(hex[i] >= '0' && hex[i] <= '9') {
+      constant = ASCIINUM;
+    } else if (hex[i] >= 'A' && hex[i] <= 'F') {
+      constant = ASCIIUPPER;
+    }  else if (hex[i] >= 'a' && hex[i] <= 'f') {
+      constant = ASCIILOWER;
+    } else {
+      error(3, errorMessage);
+      val = -1;
+      break;
+    }
+    val += scalar*((int) hex[i]-constant);
+    scalar *=16;
+  }
+  if(negative) { 
+    val *= -1;
+  }
+  return val;
+}
 
+/*
+case insensitive convert to int
+start with "-####" and convert to int
+no bounds checking yet
+*/
+int decToInt(char* dec) {
+  int i;
+  int val = 0;
+  int end = 0;
+  int scalar = 1;
+  int constant;
+  int length = strlen(dec);
+  bool negative = (dec[0] == '-');
+  if(negative) {
+    end = 1;
+  }
+  sprintf(errorMessage, "%s is a bad dec literal. Needs form of \"#-XXXXX\" - optional",dec);
 
-
+  for(i = strlen(dec)-1; i >= end; i--) {  // end skips the 'x' (and if negative '-') character(s)
+    if(dec[i] >= '0' && dec[i] <= '9') {
+      constant = ASCIINUM;
+    } else {
+      error(3, errorMessage);
+      val = -1;
+      break;
+    }
+    val += scalar*((int) dec[i]-constant);
+    scalar *=10;
+  }
+  if(negative) { 
+    val *= -1;
+  }
+  return val;
+}
 
