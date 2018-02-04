0470fb031a3a4795b8896ffebfd049dbf3f2ae3f
karlsolomon <ksolomon@utexas.edu>
2018-02-04 12:44:31 -0600

    BR->LDB

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -1,6 +1,9 @@
 #define WORDS_IN_MEM 0x08000
 #define LC_3b_REGS 8
 #define OPCODE_MASK 0xF000
+#define N 0x0800
+#define Z 0x0400
+#define P 0x0200
 
 
 typedef struct System_Latches_Struct{
@@ -31,9 +34,6 @@ enum OPCODES {
 };
 
 
-
-
-
 int MEMORY[WORDS_IN_MEM][2];
 
 /*
@@ -50,6 +50,12 @@ uint16_t maskInstruction(uint16_t instruction, uint16_t startBit, uint16_t endBi
 	return result & instruction;
 }
 
+uint16_t maskAndShiftDown(uint16_t instruction, uint16_t startBit, uint16_t endBit) {
+	instruction = maskInstruction(instruction, startBit, endBit);
+	instruction >>= startBit;
+	return instruction;
+}
+
 uint16_t readRegister(uint16_t registerNumber) {
 	return System_Latches.REGS[registerNumber];
 }
@@ -58,32 +64,35 @@ void writeRegister(uint16_t registerNumber, uint16_t newValue) {
 	System_Latches.REGS[registerNumber] = newValue;
 }
 
-uint16_t signExtend(uint16_t number, uint16_t numBits) {
-	if(number & (1 << (numBits-1))) {
-		number &= 0xFFFF;
-	} else {
-		number |= 0x0000;
+
+int16_t signExtend(uint16_t signedNumber, uint16_t bitsOccupied) {
+	int16_t result = 0;
+	if(signedNumber & (1 << (bitsOccupied-1))) { /*negative*/
+		/*take 2s compliement*/
+		signedNumber ~= signedNumber;
+		signedNumber += 1;
 	}
-	return number;
+	return (int16_t) signedNumber;
 }
 
-void writeDR(uint16_t instruction, uint16_t result) {
-	writeRegister(maskInstruction(instruction, 9,11) >> 9, result);
+void *readMemory(uint16_t startAddress, uint16_t length) {
+	/*TODO: implement, might want to make the return a void* so the user can cast the result
+	  Fairly certain this read has to be word-aligned, but not 100%.
+	  Address should only not be word-aligned if the person is looking for bytes, not words.
+	*/
 }
 
-void writeBaseR(uint16_t instruction, uint16_t result) {
-	writeRegister(maskInstruction(instruction, 6,8) >> 6, result);
-}
-
+/* likely need to handle doing operations with numbers that I SEXT, but are cast as uint16_t*/
 void execute(uint16_t instruction) {
 	uint8_t opcode = (instruction & OPCODE_MASK) >> 12;
-	uint16_t r1 = readRegister(maskInstruction(instruction, 9,11));
-	uint16_t r2 = readRegister(maskInstruction(instruction, 6,8));
-	uint16_t r3 = readRegister(maskInstruction(instruction, 0,2));
-	uint16_t bit5Mask = maskInstruction(instruction, 5,5);
-	uint16_t bit4Mask = maskInstruction(instruction, 4,4);
-	uint16_t bit11Mask = maskInstruction(instruction, 11,11);
-	uint16_t imm5 = maskInstruction(instruction, 0,4);
+	uint16_t r1 = readRegister(maskAndShiftDown(instruction, 9,11));
+	uint16_t r2 = readRegister(maskAndShiftDown(instruction, 6,8));
+	uint16_t r3 = readRegister(maskAndShiftDown(instruction, 0,2));
+	uint16_t bit5Mask = maskAndShiftDown(instruction, 5,5);
+	uint16_t bit4Mask = maskAndShiftDown(instruction, 4,4);
+	uint16_t bit11Mask = maskAndShiftDown(instruction, 11,11);
+	uint16_t imm5 = maskAndShiftDown(instruction, 0,4);
+	uint16_t PCOffset9 = maskAndShiftDown(instruction, 0, 8);
 	uint16_t result;
 	switch(opcode) {
 		case ADD:
@@ -92,19 +101,68 @@ void execute(uint16_t instruction) {
 			} else {
 				result = r2 + r3;
 			}
-			writeDR(instruction, result);
+			writeRegister(r1, result);
 		break;
 		case AND:
+			if(bit5Mask) {
+				result = r2 & signExtend(imm5);
+			} else {
+				result = r2 & r3;
+			}
+			writeRegister(r1, result);
 		break;
 		case BR_NOP:
+			/*r1 contains nzp bits*/
+			if(r1) {
+				/*BR*/
+				bool branch = false;
+				if(instruction & N) {
+					if(System_Latches.N) {
+						branch = true;
+					}
+				}
+				if(instruction & Z) {
+					if(System_Latches.Z) {
+						branch = true;
+					}
+				}
+				if(instruction & P) {
+					if(System_Latches.P) {
+						branch = true;
+					}
+				}
+				int16_t offset = signExtend(PCOffset9);
+				System_Latches.PC += (offset +1);
+			} else {
+				/*NOP*/
+			}
 		break;
-		case JMP:
+		case JMP: /*works for JMP & RET b/c in RET r2 is just R7*/
+			System_Latches.PC = r2;
 		break;
 		case JSR_R:
+			System_Latches.PC++;
+			writeRegister(7, System_Latches.PC);
+			if(instruction & (1<<11)) {
+				/*JSR*/
+				int16_t PCOffset11 = signExtend(maskAndShiftDown(instruction, 0, 10));
+				System_Latches.PC += (PCOffset11 << 1);
+			} else {
+				/*JSRR*/				
+				System_Latches.PC = r2;
+			}
 		break;
 		case LDB:
+		/*8-bit contents of memory at this address are sign-extended and stored into DR*/
+			int16_t offset = signExtend(maskAndShiftDown(instruction, 0,5));
+			uint16_t startAddress = r2 + offset;
+			uint8_t byte = 0;
+			uint8_t *memory = readMemory(startAddress, 1);
+			byte = memory[0];	//FIXME: probably has alignment issues
+			writeRegister(maskAndShiftDown(instruction, 9,11), byte);
 		break;
 		case LDW:
+
 		break;
 		case LEA:
 		break;
